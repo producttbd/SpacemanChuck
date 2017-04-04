752dc4677170827d058c71240fdec1dc781bf631
Product TBD Inc <info@producttbd.com>
2017-04-04 15:34:09 -0400

    More and better results strings
    Using SharedPreferences, the results screen now recognizes a personal
    best or the first ever toss with special strings. Also, all other
    situations have a random string selected from several possible values.

--- app/src/main/java/com/producttbd/spacemanchuck/ResultsActivity.java
+++ app/src/main/java/com/producttbd/spacemanchuck/ResultsActivity.java
@@ -1,14 +1,36 @@
 package com.producttbd.spacemanchuck;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class ResultsActivity extends AppCompatActivity {
 
     private static final String TAG = ResultsActivity.class.getSimpleName();
+    private static final Random RAND = new Random();
+    private static final double[] LEVELS = {0.25, 0.75, 2.0};
+    private static final int[] HEADLINES_LEVEL_0 = {R.string.result_headline_level_0_0,
+            R.string.result_headline_level_0_1, R.string.result_headline_level_0_2,
+            R.string.result_headline_level_0_3, R.string.result_headline_level_0_4};
+    private static final int[] HEADLINES_LEVEL_1 = {R.string.result_headline_level_1_0,
+            R.string.result_headline_level_1_1, R.string.result_headline_level_1_2,
+            R.string.result_headline_level_1_3, R.string.result_headline_level_1_4};
+    private static final int[] HEADLINES_LEVEL_2 = {R.string.result_headline_level_2_0,
+            R.string.result_headline_level_2_1, R.string.result_headline_level_2_2,
+            R.string.result_headline_level_2_3, R.string.result_headline_level_2_4};
+    private static final int[] HEADLINES_LEVEL_3 = {R.string.result_headline_level_3_0,
+            R.string.result_headline_level_3_1, R.string.result_headline_level_3_2,
+            R.string.result_headline_level_3_3, R.string.result_headline_level_3_4};
+    private static final int[] HEADLINES_PERSONAL_BEST = {R.string.result_headline_personal_best_0,
+            R.string.result_headline_personal_best_1, R.string.result_headline_personal_best_2,
+            R.string.result_headline_personal_best_3};
+
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +40,21 @@ public class ResultsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         double height = intent.getDoubleExtra(ThrowListeningActivity.THROW_RESULT_HEIGHT, 0.0);
 
+        mSharedPreferences =
+                getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+
+        float personalBestHeight = mSharedPreferences.getFloat(getString(R.string.personal_high_score_key), -1.0f);
+        boolean firstThrow = personalBestHeight < 0.0f;
+        boolean personalBest = (float) height > personalBestHeight;
+        if (personalBest) {
+            SharedPreferences.Editor editor = mSharedPreferences.edit();
+            editor.putFloat(getString(R.string.personal_high_score_key), (float) height);
+            editor.commit();
+        }
+
         // Set the reaction headline (e.g. "Good job!")
         TextView reactionHeadlineTextView = (TextView) findViewById(R.id.reactionHeadline);
-        reactionHeadlineTextView.setText(getHeadlineString(height));
+        reactionHeadlineTextView.setText(getHeadlineString(firstThrow, personalBest, height));
 
         // Set the height
         TextView heightTextView = (TextView) findViewById(R.id.heightText);
@@ -33,15 +67,23 @@ public class ResultsActivity extends AppCompatActivity {
         }
     }
 
-    private String getHeadlineString(double height) {
-        if (height < 0.25) {
-            return getString(R.string.result_level_0);
-        } else if (height < 0.75) {
-            return getString(R.string.result_level_1);
-        } else if (height < 2.0) {
-            return getString(R.string.result_level_2);
+    private String getHeadlineString(boolean firstThrow, boolean personalBest, double height) {
+        if (firstThrow) {
+            return getString(R.string.result_headline_first_throw);
+        } else if (personalBest) {
+            return getRandomString(HEADLINES_PERSONAL_BEST);
+        } else if (height < LEVELS[0]) {
+            return getRandomString(HEADLINES_LEVEL_0);
+        } else if (height < LEVELS[1]) {
+            return getRandomString(HEADLINES_LEVEL_1);
+        } else if (height < LEVELS[2]) {
+            return getRandomString(HEADLINES_LEVEL_2);
         } else {
-            return getString(R.string.result_level_3);
+            return getRandomString(HEADLINES_LEVEL_3);
         }
+    }
+
+    private String getRandomString(int[] headlines) {
+        return getString(headlines[RAND.nextInt(headlines.length)]);
     }
 }
